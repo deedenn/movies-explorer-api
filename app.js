2f9fe71c9f0a0c01dd412bfb8bf4683788947b7f
Denis Solovyev <solovyev.denis@gmail.com>
2023-07-24 23:02:00 +0300

    fix port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const { errors } = require('celebrate');
 
 const app = express();
